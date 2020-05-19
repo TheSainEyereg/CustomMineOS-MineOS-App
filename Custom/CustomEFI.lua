439cf77f4e0c0f11fea00741a7e8b7ba01ccd72f
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2020-05-19 15:03:21 +0300

    Updated and formated code

--- Custom/CustomEFI.lua
+++ Custom/CustomEFI.lua
@@ -1,1 +1,321 @@
-local a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r="MineOS EFI","Change label","key_down","filesystem",0xFFFFFF,0x000000,0xE1E1E1,0x4B4B4B,0xE1E1E1,component.proxy,component.list,computer.pullSignal,computer.uptime,table.insert,math.max,math.min,math.huge,math.floor;local s,t,u=j(k("eeprom")()),j(k("gpu")()),k("internet")()t.bind(k("screen")(),true)local v,w,x,y,z,A,B,C,D=computer.shutdown,t.set,t.setBackground,t.setForeground,t.fill,s.setData,s.getData,t.getResolution()local E,F,G,H={{"/OS.lua",function()end},{"/init.lua",function()computer.getBootAddress,computer.setBootAddress=B,A end}},function(I,J,K,L,M)x(M)z(I,J,K,L," ")end,function(J,N,O)local I=r(C/2-#O/2)y(N)w(I,J,O)end,function(O,P,Q)return{s=O,c=P,b=Q}end;local function R(J,S)J=r(D/2-J/2)F(1,1,C,D,f)G(J,e,S)return J+2 end;local function T(S,U,V)local W={}for X in U:gmatch("[^\r\n]+")do W[#W+1]=X:gsub("\t","  ")end;local J=R(#W,S)for Y=1,#W do G(J,g,W[Y])J=J+1 end;if V then repeat V=l()until V==c or V=="touch"end end;local function Z(...)local _,a0=load(...)if _ then _,a0=xpcall(_,debug.traceback)if _ then return end end;T(a,a0,1)end;local a1,a2,a3,a4=function(a5)for Y=1,#E do if a5.exists(E[Y][1])then T(a,"Booting from "..(a5.getLabel()or a5.address))if B()~=a5.address then A(a5.address)end;E[Y][2]()local a6,a7,a8,a9,a0=a5.open(E[Y][1],"rb"),""repeat a8=a5.read(a6,q)a7=a7 ..(a8 or"")until not a8;a5.close(a6)Z(a7,"="..E[Y][1])return 1 end end end,function(aa)return H("Back",aa,1)end,function(S,ab)local ac,ad=1,0;for Y=1,#ab do ad=math.max(ad,#ab[Y].s)end;while 1 do local J,I,ae=R(#ab+2,S)for Y=1,#ab do I=r(C/2-#ab[Y].s/2)if Y==ac then F(r(C/2-ad/2)-2,J,ad+4,1,h)y(i)w(I,J,ab[Y].s)x(f)else y(g)w(I,J,ab[Y].s)end;J=J+1 end;ae={l()}if ae[1]==c then if ae[4]==200 and ac>1 then ac=ac-1 elseif ae[4]==208 and ac<#ab then ac=ac+1 elseif ae[4]==28 then if ab[ac].c then ab[ac].c()end;if ab[ac].b then return end end end end end,function(J,af)local O,ag,ah,ae,ai="",true;while 1 do ah=af..O;z(1,J,C,1," ")y(g)w(r(C/2-#ah/2),J,ah..(ag and"█"or""))ae={l(0.5)}if ae[1]==c then if ae[4]==28 then return O elseif ae[4]==14 then O=O:sub(1,-2)else ai=unicode.char(ae[3])if ai:match("^[%w%d%p%s]+")then O=O..ai end end;ag=true elseif ae[1]=="clipboard"then O=O..ae[3]elseif not ae[1]then ag=not ag end end end;T(a,"Hold Alt to show boot options")local aj,ae=m()+1;while m()<aj do ae={l(aj-m())}if ae[1]==c and ae[4]==56 then local ak={H("Disk management",function()local al,am,an=function(O,ao)if#O<ao then O=O..string.rep(" ",ao-#O)else O=O:sub(1,ao)end;return O.."  "end,{a2()}local function ap()for Y=2,#am do table.remove(am,1)end;for aq in k(d)do local a5=j(aq)local ar,as,an=a5.getLabel()or"Unnamed",a5.isReadOnly(),{H("Set as bootable",function()A(aq)ap()end,1)}if not as then n(an,H(b,function()a5.setLabel(a4(R(2,b),"Enter new name: "))ap()end,1))n(an,H("Format",function()T(a,"Formatting filesystem "..aq)for at,au in ipairs(a5.list("/"))do a5.remove(au)end;ap()end,1))end;n(an,a2())n(am,1,H((aq==B()and"> "or"  ")..al(ar,12)..al(a5.spaceTotal()>1048576 and"HDD"or a5.spaceTotal()>65536 and"FDD"or"SYS",3)..al(as and"R"or"R/W",3)..al(string.format("%.1f",a5.spaceUsed()/a5.spaceTotal()*100).."%",6)..aq:sub(1,7).."…",function()a3(ar.." ("..aq..")",an)end))end end;ap()a3("Select filesystem",am)end),H("Shutdown",function()v()end),a2()}if u then n(ak,2,H("Internet recovery",function()local a6,a7,_,a0=j(u).request("https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Installer/Main.lua"),""if a6 then T(a,"Downloading recovery script")while 1 do _,a0=a6.read(q)if _ then a7=a7 .._ else a6.close()if a0 then T(a,a0,1)else Z(a7,"=string")end;break end end else T(a,"invalid URL-address",1)end end))end;a3(a,ak)end end;local a5=j(B())if not(a5 and a1(a5))then for aq in k(d)do a5=j(aq)if a1(a5)then break else a5=nil end end;if not a5 then T(a,"No bootable mediums found",1)end end;v()
+
+local stringsMain, stringsChangeLabel, stringKeyDown, stringsFilesystem, colorsTitle, colorsBackground, colorsText, colorsSelectionBackground, colorsSelectionText, componentProxy, componentList, pullSignal, uptime, tableInsert, mathMax, mathMin, mathHuge, mathFloor = "MineOS EFI", "Change label", "key_down", "filesystem", 0xFFFFFF, 0x000000, 0xE1E1E1, 0x4B4B4B, 0xE1E1E1, component.proxy, component.list, computer.pullSignal, computer.uptime, table.insert, math.max, math.min, math.huge, math.floor
+
+local eeprom, gpu, internetAddress = componentProxy(componentList("eeprom")()), componentProxy(componentList("gpu")()), componentList("internet")()
+
+gpu.bind(componentList("screen")(), true)
+
+local shutdown, gpuSet, gpuSetBackground, gpuSetForeground, gpuFill, eepromSetData, eepromGetData, screenWidth, screenHeight = computer.shutdown, gpu.set, gpu.setBackground, gpu.setForeground, gpu.fill, eeprom.setData, eeprom.getData, gpu.getResolution()
+
+local OSList, rectangle, centrizedText, menuElement =
+	{
+		{
+			"/OS.lua",
+			function()
+			end
+		},
+		{
+			"/init.lua",
+			function()
+				computer.getBootAddress, computer.setBootAddress = eepromGetData, eepromSetData
+			end
+		}
+	},
+	function(x, y, width, height, color)
+		gpuSetBackground(color)
+		gpuFill(x, y, width, height, " ")
+	end,
+	function(y, foreground, text)
+		local x = mathFloor(screenWidth / 2 - #text / 2)
+		gpuSetForeground(foreground)
+		gpuSet(x, y, text)
+	end,
+	function(text, callback, breakLoop)
+		return {
+			s = text,
+			c = callback,
+			b = breakLoop
+		}
+	end
+
+local function title(y, titleText)
+	y = mathFloor(screenHeight / 2 - y / 2)
+	rectangle(1, 1, screenWidth, screenHeight, colorsBackground)
+	centrizedText(y, colorsTitle, titleText)
+
+	return y + 2
+end
+
+local function status(titleText, statusText, needWait)
+	local lines = {}
+	for line in statusText:gmatch("[^\r\n]+") do
+		lines[#lines + 1] = line:gsub("\t", "  ")
+	end
+	
+	local y = title(#lines, titleText)
+	
+	for i = 1, #lines do
+		centrizedText(y, colorsText, lines[i])
+		y = y + 1
+	end
+
+	if needWait then
+		repeat
+			needWait = pullSignal()
+		until needWait == stringKeyDown or needWait == "touch"
+	end
+end
+
+local function executeString(...)
+	local result, reason = load(...)
+	if result then
+		result, reason = xpcall(result, debug.traceback)
+		if result then
+			return
+		end
+	end
+
+	status(stringsMain, reason, 1)
+end
+
+local boot, menuBack, menu, input =
+	function(proxy)
+		for i = 1, #OSList do
+			if proxy.exists(OSList[i][1]) then
+				status(stringsMain, "Booting from " .. (proxy.getLabel() or proxy.address))
+
+				-- Updating current EEPROM boot address if it's differs from given proxy address
+				if eepromGetData() ~= proxy.address then
+					eepromSetData(proxy.address)
+				end
+
+				-- Running OS pre-boot function
+				OSList[i][2]()
+
+				-- Reading boot file
+				local handle, data, chunk, success, reason = proxy.open(OSList[i][1], "rb"), ""
+				repeat
+					chunk = proxy.read(handle, mathHuge)
+					data = data .. (chunk or "")
+				until not chunk
+
+				proxy.close(handle)
+
+				-- Running boot file
+				executeString(data, "=" .. OSList[i][1])
+
+				return 1
+			end
+		end
+	end,
+	function(f)
+		return menuElement("Back", f, 1)
+	end,
+	function(titleText, elements)
+		local selectedElement, maxLength = 1, 0
+		for i = 1, #elements do
+			maxLength = math.max(maxLength, #elements[i].s)
+		end
+
+		while 1 do
+			local y, x, eventData = title(#elements + 2, titleText)
+			
+			for i = 1, #elements do
+				x = mathFloor(screenWidth / 2 - #elements[i].s / 2)
+				
+				if i == selectedElement then
+					rectangle(mathFloor(screenWidth / 2 - maxLength / 2) - 2, y, maxLength + 4, 1, colorsSelectionBackground)
+					gpuSetForeground(colorsSelectionText)
+					gpuSet(x, y, elements[i].s)
+					gpuSetBackground(colorsBackground)
+				else
+					gpuSetForeground(colorsText)
+					gpuSet(x, y, elements[i].s)
+				end
+				
+				y = y + 1
+			end
+
+			eventData = {pullSignal()}
+			if eventData[1] == stringKeyDown then
+				if eventData[4] == 200 and selectedElement > 1 then
+					selectedElement = selectedElement - 1
+				elseif eventData[4] == 208 and selectedElement < #elements then
+					selectedElement = selectedElement + 1
+				elseif eventData[4] == 28 then
+					if elements[selectedElement].c then
+						elements[selectedElement].c()
+					end
+
+					if elements[selectedElement].b then
+						return
+					end
+				end
+			end
+		end
+	end,
+	function(y, prefix)
+		local text, state, eblo, eventData, char = "", true
+		while 1 do
+			eblo = prefix .. text
+			gpuFill(1, y, screenWidth, 1, " ")
+			gpuSetForeground(colorsText)
+			gpuSet(mathFloor(screenWidth / 2 - #eblo / 2), y, eblo .. (state and "█" or ""))
+
+			eventData = {pullSignal(0.5)}
+			if eventData[1] == stringKeyDown then
+				if eventData[4] == 28 then
+					return text
+				elseif eventData[4] == 14 then
+					text = text:sub(1, -2)
+				else
+					char = unicode.char(eventData[3])
+					if char:match("^[%w%d%p%s]+") then
+						text = text .. char
+					end
+				end
+
+				state = true
+			elseif eventData[1] == "clipboard" then
+				text = text .. eventData[3]
+			elseif not eventData[1] then
+				state = not state
+			end
+		end
+	end
+
+status(stringsMain, "Hold Alt to show boot options")
+
+local deadline, eventData = uptime() + 1
+while uptime() < deadline do
+	eventData = {pullSignal(deadline - uptime())}
+	if eventData[1] == stringKeyDown and eventData[4] == 56 then
+		local utilities = {
+			menuElement("Disk management", function()
+				local restrict, filesystems, filesystemOptions =
+					function(text, limit)
+						if #text < limit then
+							text = text .. string.rep(" ", limit - #text)
+						else
+							text = text:sub(1, limit)
+						end
+
+						return text .. "  "
+					end,
+					{menuBack()}
+
+				local function updateFilesystems()
+					for i = 2, #filesystems do
+						table.remove(filesystems, 1)
+					end
+
+					for address in componentList(stringsFilesystem) do
+						local proxy = componentProxy(address)
+						local label, isReadOnly, filesystemOptions =
+							proxy.getLabel() or "Unnamed",
+							proxy.isReadOnly(),
+							{
+								menuElement("Set as bootable", function()
+									eepromSetData(address)
+									updateFilesystems()
+								end, 1)
+							}
+
+						if not isReadOnly then
+							tableInsert(filesystemOptions, menuElement(stringsChangeLabel, function()
+								proxy.setLabel(input(title(2, stringsChangeLabel), "Enter new name: "))
+								updateFilesystems()
+							end, 1))
+
+							tableInsert(filesystemOptions, menuElement("Format", function()
+								status(stringsMain, "Formatting filesystem " .. address)
+								
+								for _, file in ipairs(proxy.list("/")) do
+									proxy.remove(file)
+								end
+
+								updateFilesystems()
+							end, 1))
+						end
+
+						tableInsert(filesystemOptions, menuBack())
+
+						tableInsert(filesystems, 1,
+							menuElement(
+								(address == eepromGetData() and "> " or "  ") ..
+								restrict(label, 12) ..
+								restrict(proxy.spaceTotal() > 1048576 and "HDD" or proxy.spaceTotal() > 65536 and "FDD" or "SYS", 3) ..
+								restrict(isReadOnly and "R" or "R/W", 3) ..
+								restrict(string.format("%.1f", proxy.spaceUsed() / proxy.spaceTotal() * 100) .. "%", 6) ..
+								address:sub(1, 7) .. "…",
+								function()
+									menu(label .. " (" .. address .. ")", filesystemOptions)
+								end
+							)
+						)
+					end
+				end
+
+				updateFilesystems()
+				menu("Select filesystem", filesystems)
+			end),
+			
+			menuElement("Shutdown", function()
+				shutdown()
+			end),
+
+			menuBack()
+		}
+
+		if internetAddress then	
+			tableInsert(utilities, 2, menuElement("Internet recovery", function()
+				local handle, data, result, reason = componentProxy(internetAddress).request("https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Installer/Main.lua"), ""
+
+				if handle then
+					status(stringsMain, "Downloading recovery script")
+
+					while 1 do
+						result, reason = handle.read(mathHuge)	
+						
+						if result then
+							data = data .. result
+						else
+							handle.close()
+							
+							if reason then
+								status(stringsMain, reason, 1)
+							else
+								executeString(data, "=string")
+							end
+
+							break
+						end
+					end
+				else
+					status(stringsMain, "invalid URL-address", 1)
+				end
+			end))
+		end
+
+		menu(stringsMain, utilities)
+	end
+end
+
+local proxy = componentProxy(eepromGetData())
+if not (proxy and boot(proxy)) then
+	for address in componentList(stringsFilesystem) do
+		proxy = componentProxy(address)
+
+		if boot(proxy) then
+			break
+		else
+			proxy = nil
+		end
+	end
+
+	if not proxy then
+		status(stringsMain, "No bootable mediums found", 1)
+	end
+end
+
+shutdown()
